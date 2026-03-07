767ea340558ea8b5414b1f6eedb4305e8fefa2ec
Preetham M.N <preetham@Preethams-MacBook-Air.local>
2026-03-07 18:48:19 +0530

    added app icon

--- app/api.ts
+++ app/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://mina-margravial-unremittently.ngrok-free.dev/sitehire",
+  //baseURL: "https://mina-margravial-unremittently.ngrok-free.dev/sitehire",
+    baseURL: "https://hospital-nearby-backend.onrender.com/sitehire",
 });
